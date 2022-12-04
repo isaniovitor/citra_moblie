beb264402e76f900caec010148057ac0ba886348
isaniovitor <isaniovitot@gmail.com>
2022-12-04 19:05:03 -0300

    integração: tirando imagem de cadastro de usuário

--- app/src/main/java/com/example/citra_moblie/RegisterUserActivity.java
+++ app/src/main/java/com/example/citra_moblie/RegisterUserActivity.java
@@ -32,18 +32,12 @@ import com.google.firebase.database.FirebaseDatabase;
 public class RegisterUserActivity extends AppCompatActivity {
     private FirebaseAuth auth = FirebaseAuth.getInstance();
     private Button registerUserButton;
-    private int IMAGE_ACTION_CODE; // code 1 = camera; code 2 = gallery
-    private ImageView profileImage;
     private TextView registerUserName;
     private TextView registerUserEmail;
     private TextView registerUserBirthday;
     private TextView registerUserCpf;
     private TextView registerUserPassword;
     private TextView registerUserRepeatPassword;
-    private String[] necessaryPermissions = new String[]{
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,60 +52,8 @@ public class RegisterUserActivity extends AppCompatActivity {
         registerUserCpf = findViewById(R.id.typeHiringVacancyToCreate);
         registerUserPassword = findViewById(R.id.salaryVacancyToCreate);
         registerUserRepeatPassword = findViewById(R.id.registerUserRepeatPassword);
-        profileImage = findViewById(R.id.profileImage);
         registerUserButton = findViewById(R.id.announceVacancyButton);
-        ImageButton gallery = findViewById(R.id.galleryButton);
-        ImageButton camera = findViewById(R.id.cameraButton);
         IUserDAO userDAO = UserDAO.getInstance(this);
-
-        Permission.validatePermissions(necessaryPermissions, this, 1);
-        // fazer codio Caso negada a permission
-
-        // https://stackoverflow.com/questions/62671106/onactivityresult-method-is-deprecated-what-is-the-alternative
-        ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    // There are no request codes
-
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Bitmap image = null;
-
-                        try {
-                            switch (IMAGE_ACTION_CODE){
-                                case 1:
-                                    image = (Bitmap) result.getData().getExtras().get("data");
-                                    break;
-                                case 2:
-                                    Uri localImage = result.getData().getData();
-                                    image = MediaStore.Images.Media.getBitmap(getContentResolver(), localImage);
-                                    break;
-                            }
-
-                        }catch (Exception e){
-                            // por toast
-                        }
-
-                        if (image != null) {
-                            profileImage.setImageBitmap(image);
-                        }
-                    }
-                });
-
-        camera.setOnClickListener(view -> {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                resultLauncher.launch(intent);
-                IMAGE_ACTION_CODE = 1;
-            }
-        });
-
-        gallery.setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                resultLauncher.launch(intent);
-                IMAGE_ACTION_CODE = 2;
-            }
-        });
 
         registerUserButton.setOnClickListener(view -> {
             if (registerUserPassword.getText().toString().equals(registerUserRepeatPassword.getText().toString())) {
