bbdb5a86af4b5c3d875f3d95c0b8ca806b60503a
isaniovitor <isaniovitot@gmail.com>
2022-12-07 00:17:56 -0300

    clear

--- app/src/main/java/com/example/citra_moblie/dao/IUserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.citra_moblie.dao;
-
-import com.example.citra_moblie.model.User;
-
-public interface IUserDAO {
-    void createUserMock();
-    User getUser();
-    void setUser(User user);
-}
